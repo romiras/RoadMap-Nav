761baf048ecd5af61051e3d230be708b383b0e09
pgf <pgf@users.sourceforge.net>
2014-06-07 20:26:06 +0000

    basefiles: add scripts for creating the usc81070.rdm basefile
    the basefile is a rough map of country and US state borders

---
+++ roadmap/src/basefiles/basemap.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -x
+
+# the shapefiles have the same name as their directory, and no suffix
+# is needed.
+shapefiles=$(for d in ne_110m* ; do echo $d/$d ; done)
+
+# gather attribution and version info
+for s in $shapefiles
+do
+    b=$(basename $s)
+    sources="$sources$b: $(cat $s.VERSION.txt); "
+done
+
+# run all the shapefiles through the converter together
+for s in $shapefiles
+do
+    shpdump $s
+done | ./shp2osm.awk "$sources" >basefile.osm  
+
+../buildmap_osm -c ../default/All -i basefile.osm -o usc81070.rdm
+
+../buildus --path=.. --maps=.
+
+
